e4f8d970030bed5cf790849fbaf73e1defc6a7d0
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-11-01 13:53:59 -0400

    Updated from Brython Server: 11/1/2017 1:53:58 PM

--- hangmanGraphics.py
+++ hangmanGraphics.py
@@ -1,1 +1,20 @@
+#Diego Aspinwall
+#11-1-17
+#hangmanGraphics.py - the second best game ever
 
+from random import randint
+from ggame import *
+
+def pickWord(): #Should take no arguments. The function should choose a random word. My program had five random words to chose from, but yours can have more if you like.
+    
+def wordComplete(): #Should take no arguments. The function should return True if all the letters in the word have been guessed and False otherwise.
+    
+
+def printHangman(incguesses): #Should take one argument, the number of incorrect guesses. The function should print out a new part of the body based on how many wrong guesses have occurred.
+    
+
+def keyPress(event): #Should take one argument, event. The function should fill in the letter in the word if it was a correct guess and print the letter in the list of all letterers that have been guessed.
+    
+
+if __name__ == '__main__':
+    
