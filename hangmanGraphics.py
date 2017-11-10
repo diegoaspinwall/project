c31a9f978fdf03c7c6d7124967425ce9a4983c88
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-11-10 14:04:27 -0500

    Updated from Brython Server: 11/10/2017 2:04:28 PM

--- hangmanGraphics.py
+++ hangmanGraphics.py
@@ -21,7 +21,7 @@ def pickWord(): #Should take no arguments. The function should choose a random w
         return 'boolean'
 
 def wordComplete(): #Should take no arguments. The function should return True if all the letters in the word have been guessed and False otherwise.
-    letters = 1
+    letters = 0
     for ch in data['word']:
         if ch in data['guessed']:
             letters += 1
