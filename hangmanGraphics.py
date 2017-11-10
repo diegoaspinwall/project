0505bfe5266a143fa47f6a9ca74043b1a74420a8
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-11-10 14:11:19 -0500

    Updated from Brython Server: 11/10/2017 2:11:21 PM

--- hangmanGraphics.py
+++ hangmanGraphics.py
@@ -23,14 +23,14 @@ def pickWord():
 
 #Should take no arguments. The function should return True if all the letters in the word have been guessed and False otherwise.
 def wordComplete():
-    '''letters = 0
+    letters = 0
     for ch in data['word']:
         if ch in data['guessed']:
             letters += 1
         if letters == len(data['word']):
             return True
         else:
-            return False'''
+            return False
     print(len(data['word']))
 
 #Should take one argument, the number of incorrect guesses. The function should print out a new part of the body based on how many wrong guesses have occurred.
