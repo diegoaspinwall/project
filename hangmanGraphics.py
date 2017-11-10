4c228f7d7a09f794a7bcd5c8cff22da3203777e2
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-11-10 14:36:27 -0500

    Updated from Brython Server: 11/10/2017 2:36:28 PM

--- hangmanGraphics.py
+++ hangmanGraphics.py
@@ -56,7 +56,7 @@ def printHangman(incguesses):
 
 #Should take one argument, event. The function should fill in the letter in the word if it was a correct guess and print the letter in the list of all letters that have been guessed.
 def keyPress(event):
-    while data['endgame'] == 0:
+    if data['endgame'] == 0:
         #records the incorrect guesses in incguesses after every keyPress and displays the printHangman
         if event.key not in data['guessed'] and event.key not in data['word']:
             data['incguesses'] += 1
@@ -79,6 +79,12 @@ def keyPress(event):
                 place +=1
                 if ch == event.key:
                     Sprite(TextAsset(event.key,fill=black,style='bold 30pt Times'), (((60)*place-50),450))
+    
+    else:
+        place = 0
+        for ch in data['word']:
+            place += 1
+            Sprite(TextAsset(ch,fill=red,style='bold 30pt Times'), (((60)*place-50),450))
 
 
 #runs game
